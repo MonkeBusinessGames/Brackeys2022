3606dc773460b6ce9df9311dd9cac63ae3365d5b
Ahnaf Hassan <absuloutezero@gmail.com>
2022-08-22 23:58:33 -0400

    Basic Combat
    Added health, death, player attacks, and knockbacks

--- Brackeys2022/Assets/Scripts/EnemyController.cs
+++ Brackeys2022/Assets/Scripts/EnemyController.cs
@@ -7,30 +7,33 @@ public class EnemyController : MonoBehaviour
     [SerializeField] private SpriteRenderer sRend;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Animator anim;
-    [SerializeField] private float speed;
-    [SerializeField] private float idleTime;
-    [SerializeField] private float walkTime;
+    [SerializeField] private float speed = 3;
+    [SerializeField] private float idleTime = 1;
+    [SerializeField] private float walkTime = 4;
+    [SerializeField] private float knockBackForce = 5;
+    [SerializeField] private float health = 10;
+    [SerializeField] private float recoveryTime = .5f;
     private float timer;
-    private EnemyState enemyState;
+    private EnemyState state;
 
     void Start()
     {
-        enemyState = EnemyState.Idle;
+        state = EnemyState.Idle;
         timer = 0;
     }
 
     void Update()
     {
-        switch (enemyState)
+        switch (state)
         {
             case EnemyState.Idle:
                 timer += Time.deltaTime;
                 if(timer >= idleTime)
                 {
-                    enemyState = EnemyState.Walking;
+                    state = EnemyState.Walking;
                     speed *= -1;
                     sRend.flipX = !sRend.flipX;
-                    anim.SetBool("Walking", true);
+                    SetAnimation();
                     timer = 0;
                     rb.velocity = new Vector2(speed, rb.velocity.y);
                 }
@@ -39,11 +42,21 @@ public class EnemyController : MonoBehaviour
                 timer += Time.deltaTime;
                 if (timer >= walkTime)
                 {
-                    enemyState = EnemyState.Idle;
-                    anim.SetBool("Walking", false);
+                    state = EnemyState.Idle;
+                    SetAnimation();
                     rb.velocity = new Vector2(0, rb.velocity.y);
                     timer = 0;
                 }
+                break;
+            case EnemyState.Hit:
+                timer += Time.deltaTime;
+                if (timer >= recoveryTime)
+                {
+                    state = EnemyState.Idle;
+                    SetAnimation();
+                    timer = 0;
+                }
+                return;
                 break;
         }
     }
@@ -52,10 +65,57 @@ public class EnemyController : MonoBehaviour
     {
 
     }
+
+    public void TakeDamage(float damageDealt, Vector2 playerPosition)
+    {
+        state = EnemyState.Hit;
+        rb.velocity = Vector2.zero;
+        rb.AddForce(((Vector2)transform.position - playerPosition).normalized * knockBackForce, ForceMode2D.Impulse);
+        health -= damageDealt;
+        timer = 0;
+        if(health <= 0)
+            state = EnemyState.Die;
+        SetAnimation();
+    }
+
+
+    private void SetAnimation()
+    {
+        switch (state)
+        {
+            case EnemyState.Idle:
+                anim.SetInteger("State", 0);
+                break;
+            case EnemyState.Walking:
+                anim.SetInteger("State", 1);
+                break;
+            case EnemyState.Hit:
+                anim.SetInteger("State", 2);
+                break;
+/*            case EnemyState.JumpStart:
+                anim.SetInteger("State", 3);
+                break*/;
+            case EnemyState.Die:
+                anim.SetInteger("State", 4);
+                break;
+            case EnemyState.Attack:
+                anim.SetInteger("State", 5);
+                break;
+        }
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
 }
 
 public enum EnemyState
 {
     Idle,
-    Walking
+    Walking,
+    Hit,
+    Die,
+    Attack
 }
--- Brackeys2022/Assets/Scripts/PlayerMovement.cs
+++ Brackeys2022/Assets/Scripts/PlayerMovement.cs
@@ -1,28 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
 
+    //Components
     [SerializeField] private SpriteRenderer sRend;
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator anim;
+
+    //Movement Fields
     [SerializeField] private float speed;
     [SerializeField] private float jumpForce;
     [SerializeField] private BoxCollider2D groundCheck;
     [SerializeField] private LayerMask ground;
     private float movementX;
     private bool flip;
-    private JumpState jumpState;
+    private PlayerState state;
+
+    //Combat Fields
+    [SerializeField] private BoxCollider2D hitCheck;
+    [SerializeField] private ContactFilter2D enemies;
+    [SerializeField] private float health = 100;
+    [SerializeField] private float attackPower = 10;
+    [SerializeField] private float knockBackForce = 100;
+    [SerializeField] private float recoveryTime = .5f;
+    [SerializeField] private float attackTime = .5f;
+    private float timer;
+
+    [SerializeField] private TMP_Text healthText;
 
     void Start()
     {
-        jumpState = JumpState.Grounded;
+        state = PlayerState.Idle;
         flip = false;
+        timer = 0;
     }
 
     void Update()
     {
+        if (state == PlayerState.Hit)
+        {
+            timer += Time.deltaTime;
+            if(timer >= recoveryTime)
+            {
+                state = PlayerState.Idle;
+                SetAnimation();
+                timer = 0;
+            }
+            return;
+        }
+        else if (state == PlayerState.Attack)
+        {
+            timer += Time.deltaTime;
+            if (timer >= attackTime)
+            {
+                state = PlayerState.Idle;
+                SetAnimation();
+                timer = 0;
+            }
+            return;
+        }
+
         //Get Walk Input
         movementX = Input.GetAxis("Horizontal");
 
@@ -38,24 +79,41 @@ public class PlayerMovement : MonoBehaviour
                 flip = sRend.flipX = true;
         }
 
-        //Get Jump Input
-        switch (jumpState)
+        //Get Input Based on State
+        switch (state)
         {
-            case JumpState.Grounded:
+            case PlayerState.Idle:
                 //Start Jumping
                 if (Input.GetButtonDown("Jump"))
-                    jumpState = JumpState.JumpStart;
+                    state = PlayerState.JumpStart;
+                else if (Input.GetButtonDown("Attack"))
+                {
+                    state = PlayerState.Attack;
+                    SetAnimation();
+                    movementX = 0;
+                }
                 break;
-            case JumpState.JumpStart:
+            case PlayerState.Walking:
+                //Start Jumping
+                if (Input.GetButtonDown("Jump"))
+                    state = PlayerState.JumpStart;
+                else if (Input.GetButtonDown("Attack"))
+                {
+                    state = PlayerState.Attack;
+                    SetAnimation();
+                    movementX = 0;
+                }
                 break;
-            case JumpState.Jumping:
+            case PlayerState.JumpStart:
+                break;
+            case PlayerState.Jumping:
                 //Short Jump
                 if (Input.GetButtonUp("Jump"))
-                    jumpState = JumpState.JumpStop;
+                    state = PlayerState.JumpStop;
                 break;
-            case JumpState.JumpStop:
+            case PlayerState.JumpStop:
                 break;
-            case JumpState.Falling:
+            case PlayerState.Falling:
                 break;
         }
             
@@ -63,43 +121,115 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (state == PlayerState.Hit)
+            return;
 
         //Set Velocity
         rb.velocity = new Vector2(movementX * speed, rb.velocity.y);
 
-        //Handle Jump Inputs
-        switch (jumpState)
+        //State Machine
+        switch (state)
         {
-            case JumpState.Grounded:
+            case PlayerState.Idle:
+                if (rb.velocity.x != 0)
+                {
+                    state = PlayerState.Walking;
+                    SetAnimation();
+                }
                 break;
-            case JumpState.JumpStart:
+            case PlayerState.Walking:
+                if (rb.velocity.x == 0)
+                {
+                    state = PlayerState.Idle;
+                    SetAnimation();
+                }
+                break;
+            case PlayerState.JumpStart:
                 //Initiates Jump
                 rb.AddForce(new Vector2(0, jumpForce));
-                jumpState = JumpState.Jumping;
+                state = PlayerState.Jumping;
+                SetAnimation();
                 break;
-            case JumpState.Jumping:
+            case PlayerState.Jumping:
                 if (rb.velocity.y <= 0)
-                    jumpState = JumpState.Falling;
+                    state = PlayerState.Falling;
                 break;
-            case JumpState.JumpStop:
+            case PlayerState.JumpStop:
                 //Cuts Jump Short
                 rb.velocity = new Vector2(movementX * speed, rb.velocity.y / 2);
-                jumpState = JumpState.Falling;
+                state = PlayerState.Falling;
                 break;
-            case JumpState.Falling:
+            case PlayerState.Falling:
                 if(groundCheck.IsTouchingLayers(ground))
-                    jumpState = JumpState.Grounded;
+                    state = PlayerState.Idle;
                 break;
         }
+    }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Enemy")
+        {
+            if (state != PlayerState.Hit)
+            {
+                state = PlayerState.Hit;
+                SetAnimation();
+                rb.velocity = Vector2.zero;
+                rb.AddForce((transform.position - collision.transform.position).normalized * knockBackForce, ForceMode2D.Impulse);
+                health -= 1;
+                healthText.text = "Health: " + health.ToString();
+                if (health <= 0)
+                    state = PlayerState.Die;
+            }
+        }
+    }
+
+    public void HitCheck() 
+    {
+
+        List<Collider2D> hitEnemies = new List<Collider2D>();
+        print(hitCheck.OverlapCollider(enemies, hitEnemies));
+        for(int i = 0; i < hitEnemies.Count; i++)
+        {
+            hitEnemies[i].GetComponent<EnemyController>().TakeDamage(attackPower, transform.position);
+        }
+    }
+
+    private void SetAnimation()
+    {
+        switch (state)
+        {
+            case PlayerState.Idle:
+                anim.SetInteger("State", 0);
+                break;
+            case PlayerState.Walking:
+                anim.SetInteger("State", 1);
+                break;
+            case PlayerState.Hit:
+                anim.SetInteger("State", 2);
+                break;
+            case PlayerState.JumpStart:
+                anim.SetInteger("State", 3);
+                break;
+            case PlayerState.Die:
+                anim.SetInteger("State", 4);
+                break;
+            case PlayerState.Attack:
+                anim.SetInteger("State", 5);
+                break;
+        }
     }
 }
 
-public enum JumpState
+public enum PlayerState
 {
-    Grounded,
+    Idle,
+    Walking,
     JumpStart,
     Jumping,
     JumpStop,
     Falling,
+    Hit,
+    Die,
+    Attack,
 }
