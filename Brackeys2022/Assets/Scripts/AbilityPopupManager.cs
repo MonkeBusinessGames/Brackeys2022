54dcc3b2167b40d1890b14edd97018abe9558288
Arnas T <49756303+AarnasS75@users.noreply.github.com>
2022-08-28 13:59:35 +0300

    Revert "Popups BETAh"
    This reverts commit 14a2f73fd520d4f35a0619a0facf69a7457cc9b5.

--- Brackeys2022/Assets/Scripts/AbilityPopupManager.cs
+++ Brackeys2022/Assets/Scripts/AbilityPopupManager.cs
@@ -5,64 +5,16 @@ using UnityEngine.UI;
 
 public class AbilityPopupManager : MonoBehaviour
 {
-    [SerializeField] private Transform catAbilityPopup;
-    [SerializeField] private Transform moleAbilityPopup;
-    [SerializeField] private Transform birbAbilityPopup;
-    [SerializeField] private Transform doubleJumpAcquired;
+    [SerializeField] private Sprite catAbilityPopup;
+    [SerializeField] private Sprite moleAbilityPopup;
+    [SerializeField] private Sprite birbAbilityPopup;
 
     private Animator popupAnimator;
 
-    private SpriteRenderer transformImage, popupImage;
-
     private void Awake()
     {
-        transformImage = GetComponent<SpriteRenderer>();
         popupAnimator = GetComponent<Animator>();
-
-        popupAnimator.enabled = false;
-    }
-    private void Start()
-    {
-        
     }
 
-    public void ShowPopup(Popup popup)
-    {
-        switch (popup)
-        {
-            case Popup.CAT_ABILITY_ACQUIRED:
-                popupImage = catAbilityPopup.GetComponent<SpriteRenderer>();
-                transformImage = popupImage;
 
-                
-
-                break;
-            case Popup.MOLE_ABILITY_ACQUIRED:
-
-                break;
-            case Popup.BIRB_ABILITY_ACQUIRED:
-
-                break;
-            case Popup.DOUBLE_JUMP_ACQUIRED:
-
-                break;
-            case Popup.TRY_TO_HIDE_WITH_X:
-
-                break; 
-            case Popup.ATTACK_WITH_C:
-
-                break;
-        }
-        popupAnimator.Play("AbilityPopup");
-    }
-
-    public enum Popup
-    {
-        CAT_ABILITY_ACQUIRED,
-        MOLE_ABILITY_ACQUIRED,
-        BIRB_ABILITY_ACQUIRED,
-        DOUBLE_JUMP_ACQUIRED,
-        TRY_TO_HIDE_WITH_X,
-        ATTACK_WITH_C
-    }
 }
